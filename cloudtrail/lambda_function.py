e0be1939264aad9513b75a3ee1f64e87d8de8868
Mark Richman <mrichman@users.noreply.github.com>
2018-06-25 15:30:29 -0400

    Create lambda_function.py

---
+++ cloudtrail/lambda_function.py
@@ -0,0 +1,15 @@
+import boto3
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    """ Function to define Lambda Handler """
+    try:
+        client = boto3.client('cloudtrail')
+        if event['detail']['eventName'] == 'StopLogging':
+            client.start_logging(
+                Name=event['detail']['requestParameters']['name'])
+
+    except Exception, e:
+        print(e)
